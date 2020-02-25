856590ad511ab55718f2c4cc0143e49d08ad1329
Adrian Romański <muszynianka1337@gmail.com>
2020-02-25 13:06:00 +0100

    Created converters for Category

---
+++ src/main/java/adrianromanski/converters/CategoryCommandToCategory.java
@@ -0,0 +1,27 @@
+package adrianromanski.converters;
+
+
+import adrianromanski.commands.CategoryCommand;
+import adrianromanski.domain.Category;
+import jdk.internal.jline.internal.Nullable;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategoryCommandToCategory implements Converter<CategoryCommand, Category> {
+
+    @Synchronized
+    @Nullable
+    @Override
+    public Category convert(CategoryCommand source) {
+        if(source == null) {
+            return null;
+        }
+
+        final Category category = new Category();
+        category.setId(source.getId());
+        category.setDescription(source.getDescription());
+        return category;
+    }
+}
