1b29af84ab02806d6d25feb60869909ebe093d11
Adrian Romański <muszynianka1337@gmail.com>
2020-02-25 13:43:15 +0100

    Created tests for UnitOfMeasure Converters

---
+++ src/test/java/adrianromanski/converters/UnitOfMeasureCommandToUnitOfMeasureTest.java
@@ -0,0 +1,47 @@
+package adrianromanski.converters;
+
+import adrianromanski.commands.UnitOfMeasureCommand;
+import adrianromanski.domain.UnitOfMeasure;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UnitOfMeasureCommandToUnitOfMeasureTest {
+
+    private static final String DESCRIPTION = "description";
+    private static final Long LONG_VALUE = 1L;
+
+    UnitOfMeasureCommandToUnitOfMeasure converter;
+
+    @Before
+    public void setUp() throws Exception {
+        converter = new UnitOfMeasureCommandToUnitOfMeasure();
+    }
+
+    @Test
+    public void testNullParameter() {
+        assertNull(converter.convert(null));
+    }
+
+    @Test
+    public void testEmptyObject() {
+        assertNotNull(converter.convert(new UnitOfMeasureCommand()));
+    }
+
+    @Test
+    public void convert() {
+        // Given
+        UnitOfMeasureCommand command = new UnitOfMeasureCommand();
+        command.setId(LONG_VALUE);
+        command.setDescription(DESCRIPTION);
+
+        // When
+        UnitOfMeasure uoc = converter.convert(command);
+
+        // Then
+        assertNotNull(uoc);
+        assertEquals(LONG_VALUE, uoc.getId());
+        assertEquals(DESCRIPTION, uoc.getDescription());
+    }
+}
---
+++ src/test/java/adrianromanski/converters/UnitOfMeasureToUnitOfMeasureCommandTest.java
@@ -0,0 +1,47 @@
+package adrianromanski.converters;
+
+import adrianromanski.commands.UnitOfMeasureCommand;
+import adrianromanski.domain.UnitOfMeasure;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UnitOfMeasureToUnitOfMeasureCommandTest {
+
+    private static final String DESCRIPTION = "description";
+    private static final Long LONG_VALUE = 1L;
+
+    UnitOfMeasureToUnitOfMeasureCommand converter;
+
+    @Before
+    public void setUp() throws Exception {
+        converter = new UnitOfMeasureToUnitOfMeasureCommand();
+    }
+
+    @Test
+    public void testNullParameter() {
+        assertNull(converter.convert(null));
+    }
+
+    @Test
+    public void testEmptyObject() {
+        assertNotNull(converter.convert(new UnitOfMeasure()));
+    }
+
+    @Test
+    public void convert() {
+        // Given
+        UnitOfMeasure uom = new UnitOfMeasure();
+        uom.setId(LONG_VALUE);
+        uom.setDescription(DESCRIPTION);
+
+        // When
+        UnitOfMeasureCommand command = converter.convert(uom);
+
+        // Then
+        assertNotNull(command);
+        assertEquals(LONG_VALUE, command.getId());
+        assertEquals(DESCRIPTION, command.getDescription());
+    }
+}
