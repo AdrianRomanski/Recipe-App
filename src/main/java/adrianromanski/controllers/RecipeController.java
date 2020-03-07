97fdeccb775b24c5f225760777ff23f4e8d49f4e
Adrian Romański <muszynianka1337@gmail.com>
2020-03-07 13:20:08 +0100

    Showing Error Data on 404 Error Page

--- src/main/java/adrianromanski/controllers/RecipeController.java
+++ src/main/java/adrianromanski/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package adrianromanski.controllers;
 
 import adrianromanski.commands.RecipeCommand;
+import adrianromanski.exceptions.NotFoundException;
 import adrianromanski.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -55,5 +58,21 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(Exception exception) {
+
+        log.error("Handling not found exception");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+        modelAndView.addObject("exception", exception);
+
+
+        return modelAndView;
     }
 }
--- src/test/java/adrianromanski/controllers/RecipeControllerTest.java
+++ src/test/java/adrianromanski/controllers/RecipeControllerTest.java
@@ -2,6 +2,7 @@ package adrianromanski.controllers;
 
 import adrianromanski.commands.RecipeCommand;
 import adrianromanski.domain.Recipe;
+import adrianromanski.exceptions.NotFoundException;
 import adrianromanski.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
@@ -48,6 +49,16 @@ public class RecipeControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("recipe/show"))
                 .andExpect(model().attributeExists("recipe"));
+    }
+
+    @Test
+    public void testGetRecipeNotFound() throws Exception {
+
+        when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
+
+        mockMvc.perform(get("/recipe/1/show"))
+                .andExpect(status().isNotFound())
+                .andExpect(view().name("404error"));
     }
 
     @Test
